da316a3a0f094d85596e849907a9e42225d14e36
Hugo Juhel <hugo.juhel@stratemia.com>
2021-03-05 13:53:02 +0000

    refactor: improving namespace segregation for Models

--- statisfactory/__init__.py
+++ statisfactory/__init__.py
@@ -1,4 +1,4 @@
 from .catalog import Catalog  # noqa
-from .artefact_interactor import Artefact  # noqa
+from .models import Artefact, Volatile  # noqa
 from .craft import Craft  # noqa
 from .pipeline import Pipeline  # noqa
