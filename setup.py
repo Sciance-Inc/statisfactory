85c320b0ab8381ce4b75eb20ffffecc7bdbf29ec
Hugo Juhel <hugo.juhel@stratemia.com>
2021-03-27 17:19:32 +0000

    Bump version: 0.1.0-dev6 → 0.1.0-alpha0

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ README = (HERE / "readme.md").read_text()
 
 
 setup(
-    version="0.1.0-dev6",
+    version="0.1.0-alpha0",
     setup_requires=["setuptools-git-versioning"],
     name="statisfactory",
     description="Satisfying Statistical Factory",
