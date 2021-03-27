4fe6b97210c9cf168c94e7cdf4d702e689edea79
Hugo Juhel <hugo.juhel@stratemia.com>
2021-03-27 16:31:19 +0000

    chore: disabling qa on init

--- statisfactory/__init__.py
+++ statisfactory/__init__.py
@@ -2,4 +2,4 @@ from .catalog import Catalog  # noqa
 from .models import Artefact, Volatile  # noqa
 from .craft import Craft  # noqa
 from .pipeline import Pipeline  # noqa
-from .artefact_interactor import ArtefactInteractor
+from .artefact_interactor import ArtefactInteractor  # noqa
