10d39af4c27184fc4c20e8508cb71214a9fd7bce
hugo juhel <juhel.hugo@stratemia.com>
2021-10-28 18:06:56 -0400

    test: adding testing data

---
+++ tests/test_pipeline.py
@@ -0,0 +1,22 @@
+#! /usr/bin/python3
+
+# test_pipeline.py
+#
+# Project name: statisfactory
+# Author: Hugo Juhel
+#
+# description:
+"""
+    Test the ability of the pipeline to solve dependencies
+"""
+
+#############################################################################
+#                                 Packages                                  #
+#############################################################################
+
+
+def test_import():
+    """
+    Check if the library can be imported
+    """
+    from statisfactory import Session
